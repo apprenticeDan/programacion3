b211021c21738315715314a61568289e15edad33
apprenticeDan <san.ajelanrod.nadiel@gmail.com>
2025-03-21 08:59:21 -0400

    Se completó el ejercicio2. Se obtiene un array int de tamaño 6 con numeros aleatorios entre 1-100. Luego se crea un array y se asignan los elementos en orden inverso al del original.

--- gitH/programacion3/src/Ejercicio2.java
+++ gitH/programacion3/src/Ejercicio2.java
@@ -1,2 +1,32 @@
 public class Ejercicio2 {
+    int[] vector;
+
+    public Ejercicio2() {
+        vector = new int[6];
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (int) (Math.random()* 100 + 1) ;
+        }
+
+    }
+
+    public void ejecutar() {
+        int[] vector_invertido = new int[vector.length];
+        int indice = 0;
+        while (indice < vector.length){
+            vector_invertido[vector.length-1 - indice] = vector[indice];
+            indice++;
+        }
+
+        System.out.print("vector original: ");
+        for (int i = 0; i < vector.length; i++) {
+            System.out.printf("%5d", vector[i]);
+        }
+        System.out.println();
+        System.out.print("vector invertido:");
+        for (int i = 0; i < vector.length; i++) {
+            System.out.printf("%5d", vector_invertido[i]);
+        }
+        System.out.println();
+    }
+
 }
---
+++ gitH/programacion3/src/MainE2.java
@@ -0,0 +1,7 @@
+public class MainE2 {
+    public static void main(String[] args) {
+        Ejercicio2 ej = new Ejercicio2();
+        ej.ejecutar();
+    }
+
+}
