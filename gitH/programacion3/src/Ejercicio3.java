7681922c1012e71ce42842f8fa6fd5a7cf20ecf2
apprenticeDan <san.ajelanrod.nadiel@gmail.com>
2025-03-21 08:24:16 -0400

    Se completó el ejercicio3. Calcular minimo, maximo y promedio de un array de int.

---
+++ gitH/programacion3/src/Ejercicio3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    int[] notas;
+    Scanner scanner;
+
+    public Ejercicio3() {
+        notas = new int[5];
+        scanner = new Scanner(System.in);
+    }
+
+    public void ejecutar() {
+        for(int i = 0; i < notas.length; i++) {
+            System.out.printf("notas[%d]: ", i);
+            notas[i] = scanner.nextInt();
+        }
+
+        int max = notas[0];
+        int min = notas[0];
+        int suma = 0;
+        float promedio;
+
+        for (int i = 0; i < notas.length; i++) {
+            if (notas[i] > max) {
+                max = notas[i];
+            }
+            if (notas[i] < min) {
+                min = notas[i];
+            }
+            suma =  suma + notas[i];
+        }
+
+        System.out.printf("mínimo: %d%nmáximo: %d%npromedio: %.2f%n",min, max, (0f+suma)/ notas.length);
+    }
+
+}
---
+++ gitH/programacion3/src/MainE3.java
@@ -0,0 +1,6 @@
+public class MainE3 {
+    public static void main(String[] args) {
+        Ejercicio3 ej = new Ejercicio3();
+        ej.ejecutar();
+    }
+}
