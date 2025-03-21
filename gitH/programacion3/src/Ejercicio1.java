f22078dff6015a27e95dae1562f05d5d499e3279
apprenticeDan <san.ajelanrod.nadiel@gmail.com>
2025-03-21 08:45:26 -0400

    Se completó el ejercicio1. Se obtiene un array int de tamaño 10 con numeros aleatorios entre 1-10. Luego se recorre sobre sus elementos y se calculan el cuadrado y los cubos de cada uno.

--- gitH/programacion3/src/Ejercicio1.java
+++ gitH/programacion3/src/Ejercicio1.java
@@ -1,2 +1,22 @@
 public class Ejercicio1 {
+    int[] vector;
+
+    public Ejercicio1() {
+        vector = new int[10];
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (int) (Math.random()* 10 + 1) ;
+        }
+    }
+
+    public void ejecutar(){
+
+        for (int i = 0; i < vector.length; i++) {
+            int numero = vector[i];
+            int cuadrado = (int) Math.pow(numero, 2);
+            int cubo = (int) Math.pow(numero, 3);
+            System.out.printf("vector_numeros[%d]: %d, cuadrado: %d, cubo: %d%n",i,numero,cuadrado,cubo);
+        }
+    }
+
+
 }
---
+++ gitH/programacion3/src/MainE1.java
@@ -0,0 +1,6 @@
+public class MainE1 {
+    public static void main(String[] args) {
+        Ejercicio1 ej = new Ejercicio1();
+        ej.ejecutar();
+    }
+}
