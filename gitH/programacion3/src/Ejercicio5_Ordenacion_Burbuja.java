3b39e19ebca22e42da4c2dbedc286cbd8d479905
apprenticeDan <san.ajelanrod.nadiel@gmail.com>
2025-04-11 08:50:59 -0400

    Create Ejercicio5_Ordenacion_Burbuja.java

---
+++ gitH/programacion3/src/Ejercicio5_Ordenacion_Burbuja.java
@@ -0,0 +1,33 @@
+public class Ejercicio5 {
+  int[] vector;
+
+  public Ejercicio5() {
+    vector = new int[10];  
+  }
+
+  public void ejecutar() {
+    for (int i = 0; i < vector.length; i++) {
+      int aleatorio = (int)(Math.random() * 10 + 1);
+      vector[i] = aleatorio;
+    }
+
+    for (int i = 0; i < vector.length; i++) {
+      System.out.print(vector[i]+" ");
+    }
+    System.out.println();
+    
+    for (int i = 0; i < vector.length - 1; i++) {
+      for (int j = 0; j < vector.length - i -1; j++) {
+        if (vector[j] > vector[i+1]) {
+          int aux = vector[j];
+          vector[j] = vector[j+1];
+          vector[j+1] = aux;
+        }
+      }
+    }
+      for (int i = 0; i < vector.length; i++) {
+      System.out.print(vector[i]+" ");
+    }
+    System.out.println();
+  }
+}
