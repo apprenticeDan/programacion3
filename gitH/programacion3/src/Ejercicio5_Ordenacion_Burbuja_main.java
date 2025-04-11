8cab892f57a9856b9f4bf561a7502eeb4cd68610
apprenticeDan <san.ajelanrod.nadiel@gmail.com>
2025-04-11 08:53:36 -0400

    Create Ejercicio5_Ordenacion_Burbuja_main.java

---
+++ gitH/programacion3/src/Ejercicio5_Ordenacion_Burbuja_main.java
@@ -0,0 +1,7 @@
+public class Ejercicio5_Ordenacion_Burbuja_main{
+
+public static void main(String[] args) {
+    Ejercicio5_Ordenacion_Burbuja ej5 = new Ejercicio5_Ordenacion_Burbuja();
+    ej5.ejecutar();
+  }
+}
