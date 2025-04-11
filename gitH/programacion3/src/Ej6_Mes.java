fc9143898ab2b0bc576266745376a127c3e57cc9
apprenticeDan <san.ajelanrod.nadiel@gmail.com>
2025-04-11 09:27:57 -0400

    Create Ej6_Mes.java
    Clase mes con campos, numero int, nombre String y cantidad de dias int.

---
+++ gitH/programacion3/src/Ej6_Mes.java
@@ -0,0 +1,17 @@
+public class Ej6_Mes {
+  int numero;
+  String nombre;
+  int cantidadDias;
+public Ej6_Mes(int num, String nom, int dias) {
+  this.numero = num;
+  this.nombre = nom;
+  this.cantidadDias dias;
+}
+  public int getNum() { return this.numero;}
+  public String getNom() {return this.nombre;}
+  public int getDias() {return this.cantidadDias;}
+
+  public String toString() {
+    return "Mes: " + nombre + "; cantidad de Días: " cantidadDias; 
+  }
+}
