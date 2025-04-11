a239d618f4f797e30ca0cc56e61439b287bb4807
apprenticeDan <san.ajelanrod.nadiel@gmail.com>
2025-04-11 19:39:12 -0400

    Create Ejercicio10_matriz.java
    Clase que crea una matriz 5*5, asigna valores aleatorios a sus elementos, los muestra , y calcula las sumas por columnas y por filas.

---
+++ gitH/programacion3/src/Ejercicio10_matriz.java
@@ -0,0 +1,43 @@
+public class Ejercicio10_matriz {
+int[][] matriz;
+
+
+  public Ejercicio10_matriz(){
+    matriz = new int[][];
+  }
+  public void ejecutar() {
+    for (int i = 0; i < matriz.lenght; i++) {
+      for (int j = 0; j < matriz[i].lenght; j++) {
+        int ale = (int)(Math.random()*9 + 1);  
+        matriz[i][j] = ale;
+      }
+    }
+    for (int i = 0; i < matriz.lenght; i++) {
+      for (int j = 0; j < matriz[i].lenght; j++) {
+        System.out.print(matriz[i][j]+ " ");
+      }
+      System.out.println();
+    }
+
+    
+    for (int i = 0; i < matriz.lenght; i++) {
+      int suma = 0;
+      for (int j = 0; j < matriz[i].lenght; j++) {
+        suma += matriz[i][j];
+      }
+      
+      System.out.printf("suma fila %d: %d",i,suma);
+    }
+
+    for (int i = 0; i < matriz.lenght; i++) {
+      int suma = 0;
+      for (int j = 0; j < matriz[i].lenght; j++) {
+        suma += matriz[j][i];
+      }
+      
+      System.out.printf("suma columna %d: %d",j,suma);
+    }
+  }
+  
+
+}
