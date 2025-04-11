700299856d85c1cec0b7b63ea48d86db17cbdee6
apprenticeDan <san.ajelanrod.nadiel@gmail.com>
2025-04-11 10:13:20 -0400

    Create Ejercicio9.java
    Clase que pide temp medias y max, calcula temp media y ordena los dias por la temperatura minima.

---
+++ gitH/programacion3/src/Ejercicio9.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+  public class Ejercicio9 {
+  Ej9_Dia[] dias;
+  Scanner sc;
+
+    public Ejercicio9() {
+      dias = new Ej9_Dia[5];
+      sc = new Scanner(System.in);
+    }
+
+    public void ejecutar() {
+    System.out.println("almacenar temp min y max de 5 días:");
+      for (int i = 0; i < dias.length; i++) {
+        System.out.printf("tempMin[%d]: ",i);
+        double tMin = sc.nexDouble();
+
+        System.out.printf("tempMax[%d]: ",i);
+        double tMax = sc.nexDouble();
+
+        double med = (tMax+tMin)/2;
+        dias[i] = new Ej9_Dia(tMin, tMax, med);
+      }
+
+      System.out.println("temperaturas medias de 5 días:");
+      for (int i = 0; i < dias.length; i++) {
+        System.out.printf("tempMedia[%d]: %f ",i, dias[i].getTMedia());
+      }
+      system.out.println();
+
+      System.out.println("ordenar dias por temperaturas minimas:");
+      for (int i = 0; i < dias.length; i++) {
+        for (int j = 0; i < dias.length - 1 -i; j++) {
+          if(dias[j].getTMin() > dias[j+1].getTMin()) {
+            Ej9_Dia aux = dias[j];
+            dias[j] = dias[j+1];
+            dias[j+1] = aux;
+        }}
+        
+        //System.out.printf("tempMedia[%d]: %f ",i, dias[i].getTMedia());
+      }
+      system.out.println();
+System.out.println("días ordenados:");
+      for (int i = 0; i < dias.length; i++) {
+        System.out.println(dias[i]);
+      }
+    }
+}
