62376586c726e3d9038f4585d5d40cfb73665f87
apprenticeDan <san.ajelanrod.nadiel@gmail.com>
2025-04-25 09:49:55 -0400

    Update and rename gitH/programacion3/src/Ejercicio6_Mes_main.java to src/Ejercicio_06_mes/MainE6.java

--- gitH/programacion3/src/Ejercicio6_Mes_main.java
+++
@@ -1,7 +0,0 @@
-public class Ejercicio6_Mes_main{
-  public static void main(String[] args) {
-    Ejercicio6_mes ej6 = new Ejercicio6_mes();
-    ej6.ejecutar();
-  }
-  
-}
---
+++ src/Ejercicio_06_mes/MainE6.java
@@ -0,0 +1,6 @@
+public class MainE6{
+  public static void main(String[] args) {
+    Ejercicio_06 ej6 = new Ejercicio_06();
+    ej6.ejecutar();
+  }
+}
