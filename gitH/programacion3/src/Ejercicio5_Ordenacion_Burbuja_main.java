1d99c771a5be66bd7f125e6ed985e1b317962f81
apprenticeDan <san.ajelanrod.nadiel@gmail.com>
2025-04-25 09:36:26 -0400

    Update and rename gitH/programacion3/src/Ejercicio5_Ordenacion_Burbuja_main.java to parcial01/src/MainE5.java

--- gitH/programacion3/src/Ejercicio5_Ordenacion_Burbuja_main.java
+++
@@ -1,7 +0,0 @@
-public class Ejercicio5_Ordenacion_Burbuja_main{
-
-public static void main(String[] args) {
-    Ejercicio5_Ordenacion_Burbuja ej5 = new Ejercicio5_Ordenacion_Burbuja();
-    ej5.ejecutar();
-  }
-}
---
+++ parcial01/src/MainE5.java
@@ -0,0 +1,7 @@
+public class MainE5{
+
+public static void main(String[] args) {
+    Ejercicio_05 ej5 = new Ejercicio_05();
+    ej5.ejecutar();
+  }
+}
