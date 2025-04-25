a309b921fc2d93b3caa13c4511364aa3a614b9b3
apprenticeDan <san.ajelanrod.nadiel@gmail.com>
2025-04-25 09:21:41 -0400

    Update and rename Ej5_Main.java to Ej8_Main.java

--- parcial01/src/Ejercicio_08_buscaminas/Ej8_Main.java
+++ parcial01/src/Ejercicio_08_buscaminas/Ej8_Main.java
@@ -1,8 +1,8 @@
-public class Ej5_Main {
+public class Ej8_Main {
     public static void main(String[] args) {
         
         Integer[][] arr = new Integer[5][5];
-        E5_TableroBuscaMina juego = new E5_TableroBuscaMina(9,9,5);
+        Ejercicio_08 juego = new Ejercicio_08(9,9,5);
         juego.generarMinas();
         juego.contarMinasCercanas();
         juego.mostrarTablero();
